0bc45833be341ca1a0b8a3e3436f714d7d2a5efe
MikiSayaka <a8965128@gmail.com>
2014-07-26 18:35:32 +0800

    Add sample js file.

---
+++ js/sample.js
@@ -0,0 +1,7 @@
+function ListController($scope){
+  $scope.todoList = [
+    {item: 'Project-A', priority: 10, status: false, c_date: '2014/07/25'},
+    {item: 'Project-C', priority: 23, status: true, c_date: '2014/07/31'},
+    {item: 'Project-Alpha', priority: 1, status: false, c_date: '2014/09/01'}
+  ];
+}
